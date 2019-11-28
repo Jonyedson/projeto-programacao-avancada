bb47f24631c218ea3e71cda72cfdbd8bdecd7296
Cicero Jonyedson <cicerojonyedison@gmail.com>
2019-11-28 03:15:53 -0300

    Modificação das pasta de arquivos

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,12 +2,19 @@ package application;
 
 import java.util.Scanner;
 
+import model.dao.ClienteDao;
+import model.dao.DaoFactory;
+import model.entities.Cliente;
+
 public class Program {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-
+		ClienteDao clienteDao = DaoFactory.createClienteDao();
+		Cliente cliente = new Cliente("Marcos Lucio", "marcos@gmail.com", null);
+		clienteDao.insert(cliente);
+		System.out.println(cliente);
 		sc.close();
 	}
 
